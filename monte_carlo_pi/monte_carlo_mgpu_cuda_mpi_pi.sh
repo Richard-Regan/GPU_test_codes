ea5f85b97aff7a77521ca35537eeecb41a9f16fb
Richard Regan <richard.regan@durham.ac.uk>
2024-01-24 12:33:17 +0000

    Updating build scripts

--- monte_carlo_pi/monte_carlo_mgpu_cuda_mpi_pi.sh
+++ monte_carlo_pi/monte_carlo_mgpu_cuda_mpi_pi.sh
@@ -1,2 +1,10 @@
-nvcc -ccbin=mpicxx -x cu -arch=sm_70 -o monte_carlo_mgpu_cuda_mpi exercises/monte_carlo_mgpu_cuda_mpi.cpp
-mpirun -np $NUM_DEVICES ./monte_carlo_mgpu_cuda_mpi
+
+module purge
+
+module load intel_comp/2018
+module load intel_mpi/2018
+module load cuda
+
+
+nvcc -ccbin=mpicxx -x cu -arch=sm_70 -o monte_carlo_mgpu_cuda_mpi.exe monte_carlo_mgpu_cuda_mpi.cpp
+mpirun -np $NUM_DEVICES ./monte_carlo_mgpu_cuda_mpi.exe
